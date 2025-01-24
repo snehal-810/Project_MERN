ab1f95b85d9d2ba2f2f6e6cffb2230e566746f3f
atharva <jagtapatharva777@gmail.com>
2025-01-24 14:18:16 +0530

    Admin Registration Completed

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -15,7 +15,26 @@ const GROUP_TABLE = config.GROUP_TABLE_NAME;
 const STUDENT_TABLE = config.STUDENT_TABLE_NAME;
 const MARKS_ENTRY_TABLE = config.MARK_ENTER_TABLE_NAME;
 
+// ADMIN REGISTER 
 
+router.post("/register", (request, response) =>{
+  const {email, password, employee_number, staff_name, role} = request.body;
+
+  const statement = `INSERT INTO ${STAFF_TABLE} (email, password,employee_number, staff_name, role)
+  VALUES (?, ?, ?, ?, ?)`;
+
+  const encryptedPassword = String(crypto.SHA256(password));
+
+  db.execute(
+    statement, [email, encryptedPassword, employee_number, staff_name, role],
+    (error, result) => {
+      if(error){
+        console.log("error", error)
+      }
+      response.send(utils.createResponse(error, result));
+    }
+  );
+});
 
 
   module.exports = router;
